443154e7ac6f5cc9f377aef89a227d0ec1f3ef3c
harivivek22 <nshv.damaraju@gmail.com>
2025-03-17 09:58:30 -0500

    Add files via upload

---
+++ components/footer.tsx
@@ -0,0 +1,8 @@
+export function Footer() {
+  return (
+    <footer className="w-full py-4 text-center bg-gray-800 text-white">
+      <p>Order of Movies - A fun game to test your movie knowledge!</p>
+    </footer>
+  )
+}
+
